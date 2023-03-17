89d3bad5107b3b48e584e177be87d987973c77d0
faces-of-eth <facesofeth@proton.me>
2023-03-17 08:38:27 -0700

    Try to make things better for future hustlers

--- packages/api/grifts/database.go
+++ packages/api/grifts/database.go
@@ -9,13 +9,22 @@ import (
 )
 
 var _ = g.Namespace("db", func() {
-	g.Desc("bootstrap", "Boostraps the database using ENT models")
+	g.Desc("bootstrap", "Boostraps the database using ENT models. DOES NOT SET EVERYTHING SEE COMMENTS ON THIS METHOD")
 	g.Add("bootstrap", func(c *g.Context) error {
 		fmt.Println("BOOTSTRAPPING DATABASE")
 		b.Bootstrap()
 		g.Run("paper:seed_wallets", c)
 		g.Run("gear:set_metrics", c)
 		g.Run("gear:sync_claims", c)
+		// WARNING TODO:
+		//
+		// This does not set everything, like the "sprite sheet" for each body_part
+		// in the system. There is a separate script that does that.
+		//
+		// THINGS WILL BREAK IF YOU RUN THIS WITHOUT RUNNING THAT SCRIPT
+		// Meaning, our MMO might not show characters, or the website "flex"
+		// page might not show an animation at all.
+		//
 		// g.Run("hustlers:sync", c)
 		return nil
 	})
